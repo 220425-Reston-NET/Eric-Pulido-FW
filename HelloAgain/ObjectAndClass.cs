719894797b1e5936b4439d504144e75c6ddee50a
Phreshh <jpulido916@yahoo.com>
2022-04-27 11:10:37 -0700

    Objects

---
+++ HelloAgain/ObjectAndClass.cs
@@ -0,0 +1,90 @@
+//this is how you creta a class by using the class keyword
+//Public is just there to make the class available for everyone.
+public class Car
+{
+    //This is a field
+    //its used to store information or used to define the current state of the object.
+    //By default , fields should all be private
+    private string _color= "red";
+    private string _owner = "eric";
+
+    private int _fuel = 100;
+
+    private int _gallonPerMile = 5;
+
+    //This is a method
+    //A way to represent behavior/function and it will create multiple lines of code.
+    public int TotalDistancePerFuel()
+    {
+        Console.WriteLine("Current Fuel:" +_fuel);
+        Console.WriteLine("Gallon Per Mile:" +_gallonPerMile);
+
+        Console.WriteLine(_fuel/_gallonPerMile + "This is how many miles");
+
+        //return keyworn is what the method will give back
+        return _fuel/_gallonPerMile;
+    }
+
+    //Methods can also have parameters
+    //parameters are implemented by adding in datatypes of the paenthesis
+    public void Sum(int num1, int num2)
+    {
+        Console.WriteLine(num1+num2);
+    }
+
+    //this is a constructor
+    //It is a special method that will run when you create an object
+
+    public Car()
+    {
+        _color = "Blue";
+        _fuel = 100;
+        _gallonPerMile = 5;
+        _owner = "eric";
+    }
+
+    public Car(string p_owner)
+    {
+        _owner = p_owner;
+    }
+    //This is a property
+    //  Gives us the flexibility to check that the data stored is correct, let only things access data, makes things read or write
+    public string Owner
+    {
+        //get keyword is this is how the data is going to be shared
+        get {return _owner + "is the new owner";}
+
+        //set keyword is how you change the data that is being stored
+        set {_owner=value +" Owner";}
+    }
+    public int Fuel
+    {
+        get {return _fuel;}
+        set 
+        {
+            if (value <=100 && value >= 0)
+            {
+                _fuel = value;
+            }
+            else    
+            {
+                Console.WriteLine("Fuel can only hold from 0-100");
+            }
+        }
+    }
+}
+    //This is a constructor
+    //It is a method that will execute when you create an object a 
+   // public Car(string p_color)
+    //{
+       // _color = p_color;
+    //}
+    //This is a property
+    //public string Color 
+    //{
+        //How can i grab this information
+    //    get {return _color + "is this the color of the car";}
+  //How can i save this
+    //    set {}
+  //  }
+//}
--- HelloAgain/Program.cs
+++ HelloAgain/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.Clear(); //Will clear the console
 Console.WriteLine("Hello, World!");
+
+
+
+//[class name] [NameOfObject] = new [Class name];
+Car carObj1 = new Car();
+
+
+
+
+//refencing class members within an object
+int mile = carObj1.TotalDistancePerFuel();
+
+carObj1.Sum(5,10);
+
+Console.WriteLine(mile);
+
+Console.WriteLine("End of method");
+string owner = "fred";
+Car carObj2 = new Car(owner);
+Console.WriteLine(carObj2.Owner);
+carObj2.Owner = "Daniel";
+Console.WriteLine(carObj2.Owner);
+carObj2.Fuel = 100;
+Console.WriteLine(carObj2.Fuel);
