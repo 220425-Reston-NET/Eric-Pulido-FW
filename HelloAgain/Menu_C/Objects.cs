59b3b5b8120a7124e8c37bcee2ddc96ac74de314
Phreshh <jpulido916@yahoo.com>
2022-05-02 19:17:31 -0700

    HelloAgain

--- HelloAgain/Menu_C/Objects.cs
+++ HelloAgain/Menu_C/Objects.cs
@@ -1,11 +1,4 @@
 public class Menu
 {
-    private string _grain = "Bread";
-
-    private string _dairy = "Milk";
-
-    private string _meat = "Bacon";
-
-    private string _dairy2 = "Cheese";
 }
 
--- HelloAgain/ObjectAndClass.cs
+++ HelloAgain/ObjectAndClass.cs
@@ -2,29 +2,6 @@
 //Public is just there to make the class available for everyone.
 public class Menu
 {
-    //This is a field
-    //its used to store information or used to define the current state of the object.
-    //By default , fields should all be private
-   
-    //This is a method
-    //A way to represent behavior/function and it will create multiple lines of code.
-    public int TotalDistancedotPerFuel()
-    {
-        Console.WriteLine("Current Fuel:" +_fuel);
-        Console.WriteLine("Gallon Per Mile:" +_gallonPerMile);
-
-        Console.WriteLine(_fuel/_gallonPerMile + "This is how many miles");
-
-        //return keyworn is what the method will give back
-        return _fuel/_gallonPerMile;
-    }
-
-    //Methods can also have parameters
-    //parameters are implemented by adding in datatypes of the paenthesis
-    public void Sum(int num1, int num2)
-    {
-        Console.WriteLine(num1+num2);
-    }
 
     //this is a constructor
     //It is a special method that will run when you create an object
@@ -40,31 +17,6 @@ public class Menu
     public Car(string p_owner)
     {
         _owner = p_owner;
-    }
-    //This is a property
-    //  Gives us the flexibility to check that the data stored is correct, let only things access data, makes things read or write
-    public string Owner
-    {
-        //get keyword is this is how the data is going to be shared
-        get {return _owner + "is the new owner";}
-
-        //set keyword is how you change the data that is being stored
-        set {_owner=value +" Owner";}
-    }
-    public int Fuel
-    {
-        get {return _fuel;}
-        set 
-        {
-            if (value <=100 && value >= 0)
-            {
-                _fuel = value;
-            }
-            else    
-            {
-                Console.WriteLine("Fuel can only hold from 0-100");
-            }
-        }
     }
 }
     //This is a constructor
